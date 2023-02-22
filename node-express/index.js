51a2bf5cbc58aa3b3c3d7337e2ab00842bd16862
omar alfuhaidi <omaralmunssoe@gmail.com>
2023-02-22 22:37:33 +0300

    Express Generator

--- node-express/index.js
+++ node-express/index.js
@@ -5,6 +5,9 @@ const bodyParser=require('body-parser');
 
 const dishRouter=require('./routes/dishRouter');
 
+const promoRouter=require('./routes/promoRouter');
+const leaderRouter=require('./routes/leaderRouter');
+
 const hostname='localhost';
 const port=3000;
 
@@ -13,30 +16,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes',dishRouter);
-
-
-
-
-
-   /* app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});*/
-
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 
 app.use(express.static(__dirname+ '/public'))
