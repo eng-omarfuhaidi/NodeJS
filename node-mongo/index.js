f035e01515f4730ee6aa51954a0aa615be757598
omar alfuhaidi <omaralmunssoe@gmail.com>
2023-02-25 23:25:59 +0300

    Node MongoDB Example2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient =require('mongodb').MongoClient;
 const assert= require('assert');
+const dboper=require('./operations');
 
 const url ='mongodb://localhost:27017/';
 
@@ -13,20 +14,24 @@ MongoClient.connect(url,(err,client)=>{
 
     const db =client.db(dbname);
 
-    const collection = db.collection('dishes');
-    collection.insertOne({"name":"Petza","Discription":"Test"},(err,result)=>{
+    dboper.insertDocument(db,{name:"Vadonut",desciption:'Test'},'dishes',(result)=>{
+        console.log('Insert Document :\n',result.ops);
+        dboper.findDocuments(db,'dishes',(docs)=>{
+            console.log('Found Document :\n' , docs);
 
-        assert.equal(err,null);
+            dboper.updateDocument(db,{name:'Vadonut'},{desciption:'Update Test'},'dishes',(result)=>{
+                console.log('Updated Document ',result.result);
 
-        console.log('After insert:\n');
-        console.log(result.ops);
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
-            console.log('Find:\n');
-            console.log(docs);
+                dboper.findDocuments(db,'dishes',(docs)=>{
+                    console.log('Found Document :\n' , docs);
+                db.dropCollection('dishes',(result)=>{
+                    console.log('Drop collection : ' , result);
+                    client.close();
+                });
+                });
 
-
+            });
         });
-
     });
+  
 });
