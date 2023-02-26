f12ccafef010ec79ee50e2e0cfdb2a820ec886ce
omar alfuhaidi <omaralmunssoe@gmail.com>
2023-02-26 15:51:45 +0300

    Mongoose Part One

---
+++ node-mongoose/index.js
@@ -0,0 +1,31 @@
+const mongoose =require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/data';
+
+const connect = mongoose.connect(url);
+
+connect.then((db)=>{
+console.log('Connected Correctly to a server');
+
+var newDish= Dishes ({
+name:"Delicious Petza",
+description:"Testing Petza"
+});
+
+newDish.save().then((dish)=>{
+console.log(dish);
+return Dishes.find({}).exec();
+})
+.then((dishes)=>{
+    console.log(dishes);
+    return Dishes.deleteMany({});
+})
+.then(()=>{
+    return mongoose.connection.close();
+})
+.catch((err)=>{
+    console.log(err);
+});
+});
